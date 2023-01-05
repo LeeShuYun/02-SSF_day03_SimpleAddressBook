3781f42de579476a851fc099140011659d69c97d
LeeShuyun <quaternion.design@gmail.com>
2023-01-05 09:27:36 +0800

    first commit

---
+++ src/main/java/com/addressbook/simpleAddressBook/SimpleAddressBookApplication.java
@@ -0,0 +1,13 @@
+package com.addressbook.simpleAddressBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleAddressBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleAddressBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/addressbook/simpleAddressBook/SimpleAddressBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.addressbook.simpleAddressBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleAddressBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
